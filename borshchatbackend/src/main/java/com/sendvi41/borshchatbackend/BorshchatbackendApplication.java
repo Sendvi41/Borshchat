6f1889c3cba78ad2817033452c633d2302dfc8cc
Semyon <sendvi41@gmail.com>
2020-11-05 00:47:48 +0300

    First commit

---
+++ borshchatbackend/src/main/java/com/sendvi41/borshchatbackend/BorshchatbackendApplication.java
@@ -0,0 +1,13 @@
+package com.sendvi41.borshchatbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BorshchatbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BorshchatbackendApplication.class, args);
+    }
+
+}
