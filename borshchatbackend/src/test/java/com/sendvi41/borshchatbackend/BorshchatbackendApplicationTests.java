ff844ee11ada1c6af9540c7a50a6ba12958be997
Semyon <sendvi41@gmail.com>
2021-02-03 17:39:35 +0300

    add test and properties

---
+++ borshchatbackend/src/test/java/com/sendvi41/borshchatbackend/BorshchatbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sendvi41.borshchatbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BorshchatbackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
