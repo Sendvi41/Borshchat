baf147539b19d6cb3e6522f6ff4aa8445c9724e4
Semyon <sendvi41@gmail.com>
2021-02-16 19:28:04 +0300

    add cache

--- borshdesk/src/main/java/com/sendvi41/borshdesk/BorshchatdeskApplication.java
+++ borshdesk/src/main/java/com/sendvi41/borshdesk/BorshchatdeskApplication.java
@@ -7,8 +7,9 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
-
+@EnableCaching
 @SpringBootApplication
 public class BorshchatdeskApplication extends Application {
     private static String[] args;
