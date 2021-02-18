3238d1c677e7c670d19e2bdff1b044bc5ed3d424
Semyon <sendvi41@gmail.com>
2021-02-18 20:03:49 +0300

    update entity

--- borshchatbackend/src/main/java/com/sendvi41/borshchatbackend/entities/Consultant.java
+++ borshchatbackend/src/main/java/com/sendvi41/borshchatbackend/entities/Consultant.java
@@ -7,6 +7,7 @@ import lombok.Data;
 
 import javax.persistence.*;
 import java.util.List;
+import java.util.Set;
 
 @Entity
 @Table(name = "consultants")
@@ -31,7 +32,7 @@ public class Consultant{
 
     @JsonProperty("template_id")
     @OneToMany(fetch = FetchType.EAGER, mappedBy = "consultant_id")
-    private List<Template> templates;
+    private Set<Template> templates;
 
 
 }
