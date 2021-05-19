26c21172959c3ac0864528a6e9cd8aad45e56387
Semyon <sendvi41@gmail.com>
2021-05-20 01:50:07 +0300

    update style for task

--- borshchatbackend/src/main/frontend/main/task.js
+++ borshchatbackend/src/main/frontend/main/task.js
@@ -28,36 +28,31 @@ export default class Task extends Component {
             <div>
                 <br/>
                 <br/>
+                <h2 className="title">Task#id {this.state.id} </h2>
+                <label className="labelbold">{this.state.task.theme}</label>
                 <div className="task_form">
-                    <ul>
-                        <li>
-                            <h2>Task#id {this.state.id} </h2>
-                            <span className="labelbold">{this.state.task.theme}</span>
-                        </li>
+
                         <div className="size_block">
-                        <li>
-                            <label>Client's name: {this.state.task.nameclient} </label>
-                            <label>Date: {new Date(Date.parse(this.state.task.date + "0000")).toDateString()}
-                                {new Date(Date.parse(this.state.task.date + "0000")).toTimeString()}</label>
-                        </li>
-                        <li>
-                            <label>Client's surname: {this.state.task.surnameclient}</label>
-                            <label>Author: {this.state.task.consultant_id && this.state.task.consultant_id.name}</label>
-                        </li>
-                        <li>
-                            <label>Client's patronymic: {this.state.task.patronymicclient}</label>
+
+                            <label>Client's name: <span>{this.state.task.nameclient}</span> </label>
+                            <label>Client's surname: <span>{this.state.task.surnameclient}</span> </label>
+                            <label>Client's patronymic: <span>{this.state.task.patronymicclient}</span></label>
+                            <label>E-mail:<span> {this.state.task.email}</span></label>
+                            <label>Priority of task: <span>{this.state.task.priority}</span></label>
+                            <label>Date: <span>{new Date(Date.parse(this.state.task.date + "0000")).toDateString()}
+                                {new Date(Date.parse(this.state.task.date + "0000")).toTimeString()}</span></label>
+
+                            <label>Author:<span> {this.state.task.consultant_id && this.state.task.consultant_id.name}</span></label>
+
                             <label>Consult's
-                                id: {this.state.task.consultant_id && this.state.task.consultant_id.id}</label>
-                        </li>
-                        <li>
-                            <label>E-mail: {this.state.task.email}</label>
-                            <label>Tracker: {this.state.task.tracker}</label>
-                        </li>
-                        <li>
-                            <label>Priority of task: {this.state.task.priority}</label>
-                        </li>
+                                id: <span>{this.state.task.consultant_id && this.state.task.consultant_id.id}</span></label>
+
+                            <label>Tracker: <span>{this.state.task.tracker}</span></label>
+                            <label>Status: <span>{this.state.task.status}</span></label>
+
+
                         </div>
-                    </ul>
+
                     <label className="description">Description</label>
                     <br/>
                     <label className="underline">{this.state.task.comment}</label>
