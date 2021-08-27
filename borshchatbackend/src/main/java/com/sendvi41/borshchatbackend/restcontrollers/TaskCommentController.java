2c60d7c9b60f971f326289597fc58f695abcc14c
Semyon <sendvi41@gmail.com>
2021-08-27 09:15:33 +0300

    add test commit for test breanch

--- borshchatbackend/src/main/java/com/sendvi41/borshchatbackend/restcontrollers/TaskCommentController.java
+++ borshchatbackend/src/main/java/com/sendvi41/borshchatbackend/restcontrollers/TaskCommentController.java
@@ -15,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,6 +23,7 @@ import java.util.stream.Collectors;
 
 
 @RestController
+@Controller
 @RequestMapping("/taskcomment")
 public class TaskCommentController {
 
